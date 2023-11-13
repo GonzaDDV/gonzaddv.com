66a27a0ff5c5b442d0fee1894b9a0845fc322f0a
GonzaDDV <gonzalodiazdevivar@gmail.com>
2023-11-13 14:17:48 -0300

    messipage

---
+++ src/pages/messipage.tsx
@@ -0,0 +1,22 @@
+export default function MessiPage() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-medium text-center">
+        This domain is for sale! <br /> Contact me at{" "}
+        <a
+          href="https://twitter.com/gonzaddv"
+          className="text-cyan-500 underline underline-offset-4"
+        >
+          @gonzaddv
+        </a>{" "}
+        on X/Twitter or{" "}
+        <a
+          href="mailto:gonzalodiazdevivar@gmail.com"
+          className="text-cyan-500 underline underline-offset-4"
+        >
+          gonzalodiazdevivar@gmail.com
+        </a>
+      </h1>
+    </div>
+  );
+}
